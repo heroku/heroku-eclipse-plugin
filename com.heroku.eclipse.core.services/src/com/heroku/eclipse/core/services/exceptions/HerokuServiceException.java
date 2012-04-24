ad2f0e4f90c6dcb363989b6f54e18e8a0b8dcfc5
Tom Schindl <tom.schindl@bestsolution.at>
2012-04-24 19:12:03 +0200

    adding junit-test plugin for core.services

--- com.heroku.eclipse.core.services/src/com/heroku/eclipse/core/services/exceptions/HerokuServiceException.java
+++ com.heroku.eclipse.core.services/src/com/heroku/eclipse/core/services/exceptions/HerokuServiceException.java
@@ -1,5 +1,36 @@
 package com.heroku.eclipse.core.services.exceptions;
 
 public class HerokuServiceException extends Exception {
-
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static final int UNKNOWN_ERROR = 0;
+	
+	public static final int LOGIN_FAILED_ERROR_CODE = 1;
+	
+	private final int errorCode;
+	
+	public HerokuServiceException(Throwable t) {
+		this(UNKNOWN_ERROR, t);
+	}
+	
+	public HerokuServiceException(int errorCode, Throwable t) {
+		super(t);
+		this.errorCode = errorCode;
+	}
+	
+	public HerokuServiceException(String message, Throwable t) {
+		this(UNKNOWN_ERROR, message, t);
+	}
+	
+	public HerokuServiceException(int errorCode, String message, Throwable t) {
+		super(message, t);
+		this.errorCode = errorCode;
+	}
+	
+	public int getErrorCode() {
+		return errorCode;
+	}
 }
