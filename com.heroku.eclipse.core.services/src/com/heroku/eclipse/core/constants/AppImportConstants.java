8e11722e506d2a7afdc2721062e35ace9e2dd0f7
daudo <udo.rader@bestsolution.at>
2012-05-21 16:04:19 +0200

    globalizing widget identifiers for app creation an import

--- com.heroku.eclipse.core.services/src/com/heroku/eclipse/core/constants/AppImportConstants.java
+++ com.heroku.eclipse.core.services/src/com/heroku/eclipse/core/constants/AppImportConstants.java
@@ -8,7 +8,7 @@ package com.heroku.eclipse.core.constants;
  * used for service, GUI and testing purposes 
  * @author udo.rader@bestsolution.at
  */
+@SuppressWarnings("javadoc")
 public class AppImportConstants {
-	public static final String V_APPS_LIST = "importAppsList";
-
+	public static final String V_APPS_LIST = "importAppsList"; //$NON-NLS-1$
 }
