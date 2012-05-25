9b1589419f638202ceb497c6b5525b4e6636b12f
Tom Schindl <tom.schindl@bestsolution.at>
2012-05-25 14:29:39 +0200

    implemented showing of collaborators

--- com.heroku.eclipse.ui/src/com/heroku/eclipse/ui/views/dialog/CollaboratorsPart.java
+++ com.heroku.eclipse.ui/src/com/heroku/eclipse/ui/views/dialog/CollaboratorsPart.java
@@ -1,5 +1,6 @@
 package com.heroku.eclipse.ui.views.dialog;
 
+import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
@@ -9,9 +10,17 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
 import com.heroku.api.App;
+import com.heroku.api.Collaborator;
+import com.heroku.eclipse.core.services.exceptions.HerokuServiceException;
+import com.heroku.eclipse.ui.Activator;
+import com.heroku.eclipse.ui.utils.HerokuUtils;
+import com.heroku.eclipse.ui.utils.LabelProviderFactory;
+import com.heroku.eclipse.ui.utils.RunnableWithReturn;
+import com.heroku.eclipse.ui.utils.ViewerOperations;
 
 public class CollaboratorsPart {
 	private TableViewer viewer;
+	private App domainObject;
 
 	public Composite createUI(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
@@ -22,17 +31,26 @@ public class CollaboratorsPart {
 			viewer.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
 			viewer.getTable().setHeaderVisible(true);
 			viewer.getTable().setLinesVisible(true);
+			viewer.setContentProvider(new ArrayContentProvider());
 			
 			{
 				TableViewerColumn column = new TableViewerColumn(viewer, SWT.NONE);
 				column.getColumn().setText("Owner");
-				column.getColumn().setWidth(200);
+				column.getColumn().pack();
+				column.setLabelProvider(LabelProviderFactory.createCollaborator_Owner(new RunnableWithReturn<Boolean, Collaborator>() {
+					
+					@Override
+					public Boolean run(Collaborator argument) {
+						return domainObject.getOwnerEmail() == null ? Boolean.FALSE : domainObject.getOwnerEmail().equals(argument.getEmail());
+					}
+				}));
 			}
 
 			{
 				TableViewerColumn column = new TableViewerColumn(viewer, SWT.NONE);
 				column.getColumn().setText("Collaborator E-Mail");
 				column.getColumn().setWidth(200);
+				column.setLabelProvider(LabelProviderFactory.createCollaborator_Email());
 			}			
 		}
 		
@@ -69,6 +87,13 @@ public class CollaboratorsPart {
 	}
 	
 	public void setDomainObject(App domainObject) {
-		
+		try {
+			this.domainObject = domainObject; 
+			HerokuUtils.runOnDisplay(true, viewer, Activator.getDefault().getService().getCollaborators(domainObject),
+					ViewerOperations.input(viewer));
+		} catch (HerokuServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
