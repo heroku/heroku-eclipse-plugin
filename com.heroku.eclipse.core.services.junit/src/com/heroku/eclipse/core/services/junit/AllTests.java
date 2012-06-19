7330e47f235fb5d826f45aef9850b8aa3a93a36c
daudo <udo.rader@bestsolution.at>
2012-06-19 17:44:38 +0200

    reenabling junit tests

--- com.heroku.eclipse.core.services.junit/src/com/heroku/eclipse/core/services/junit/AllTests.java
+++ com.heroku.eclipse.core.services.junit/src/com/heroku/eclipse/core/services/junit/AllTests.java
@@ -6,11 +6,11 @@ import junit.framework.TestSuite;
 public class AllTests {
 	public static Test suite() {
 		TestSuite s = new TestSuite("Core Services Tests"); //$NON-NLS-1$
-//		s.addTestSuite(HerokuServiceSimpleTest.class);
+		s.addTestSuite(HerokuServiceSimpleTest.class);
 		s.addTestSuite(HerokuServiceOwnAppTest.class);
 		s.addTestSuite(HerokuServiceForeignAppTest.class);
 		
-//		s.addTestSuite(HerokuSessionSimpleTest.class);
+		s.addTestSuite(HerokuSessionSimpleTest.class);
 		return s;
 	}
 }
