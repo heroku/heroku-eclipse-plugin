99eb166bbabdf189e0daa6196ddc76a4e268c1f5
daudo <udo.rader@bestsolution.at>
2012-07-27 14:06:48 +0200

    javadoc fixes

--- heroku-eclipse-useragent/src/main/java/com/heroku/eclipse/useragent/EclipseUseragent.java
+++ heroku-eclipse-useragent/src/main/java/com/heroku/eclipse/useragent/EclipseUseragent.java
@@ -2,18 +2,28 @@ package com.heroku.eclipse.useragent;
 
 import com.heroku.api.http.UserAgentValueProvider;
 
+/**
+ * UserAgentValueProvider used to identify the Eclipse client to the heroku services. Get repackaged in the com.heroku.api bundle.
+ * @author tom.schindl@bestsolution.at
+ */
 public class EclipseUseragent extends UserAgentValueProvider.DEFAULT {
 	private final String localUserAgent;
 	
+	/**
+	 * 
+	 */
 	public EclipseUseragent() {
-		this.localUserAgent = "heroku-eclipse-plugin v/1.0";
+		this.localUserAgent = "heroku-eclipse-plugin v/1.0"; //$NON-NLS-1$
 	}
 	
+	/**
+	 * @return the user agent string
+	 */
 	public String getLocalUserAgent() {
         return localUserAgent;
     }
 
     public String getHeaderValue(String customPart) {
-        return localUserAgent + " " + super.getHeaderValue(customPart);
+        return localUserAgent + " " + super.getHeaderValue(customPart); //$NON-NLS-1$
     }
 }
